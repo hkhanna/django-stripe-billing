41d0869d1b0c3ba7da36ab0a648d6814b4c50c1c
Harry Khanna <harry@khanna.cc>
2022-02-16 23:34:42 -0500

    Remove unnecessary i18n

--- billing/tests/settings.py
+++ billing/tests/settings.py
@@ -39,15 +39,7 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
-USE_I18N = True
-
 USE_TZ = True
 
 
