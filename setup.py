5a482229813e974ed0496cc0398d1ea7be308afb
Harry Khanna <harry@khanna.cc>
2022-02-17 11:17:04 -0500

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+import setuptools
 
-setup()
+setuptools.setup(
+    name="django-stripe-billing",
+    version="0.3.6",
+    author="Harry Khanna",
+    author_email="harry@khanna.cc",
+    description="Django billing plans and payments with Stripe",
+    license="MIT",
+    url="https://github.com/hkhanna/django-stripe-billing",
+    packages=setuptools.find_packages(),
+    install_requires=["Django", "stripe==2.*"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+    ],
+)
